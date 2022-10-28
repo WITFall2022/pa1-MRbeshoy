855db88075a3aa55a9a2ef1f3ab7765c13bd9740
hp <hp@BeshoyLaptop.schools.mybps.org>
2022-10-28 11:22:58 -0400

    I hope this works

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,11 +1,23 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter number of inches in whole numbers:");
+		int inches = input.nextInt();
+		int yards = inches/36;
+		inches = inches - (36*yards);
+		int feet = inches/12;
+		inches = inches - (12*feet);
+		System.out.println("Yards:"+yards);
+		System.out.println("feet:"+feet);
+		System.out.println("inches:"+inches);
 	}
 
 }
